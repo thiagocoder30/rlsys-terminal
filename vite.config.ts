de7d42f356cacbe6723a6d4511bb22dcfdb83b8a
thiagocoder30 <thiagocoder30@gmail.com>
2026-03-21 17:56:24 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// ==========================================
-// INJEÇÃO BLINDADA (BYPASS DO TERMUX .ENV)
-// ==========================================
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify('AIzaSyB2J45k42lGZfLxqsO8n9Gpf2b7w4Fx9PI'),
-    'import.meta.env.VITE_GEMINI': JSON.stringify('AIzaSyB2J45k42lGZfLxqsO8n9Gpf2b7w4Fx9PI')
-  }
+  plugins: [react()]
 });
